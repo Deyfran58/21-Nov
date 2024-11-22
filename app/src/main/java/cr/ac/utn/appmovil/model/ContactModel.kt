4bf0ea79cfeb3b60b3b030684c35e47b6b4f6efb
Deyfran58 <dearagonca@est.utn.ac.cr>
2024-11-21 20:22:25 -0600

    Listo

--- app/src/main/java/cr/ac/utn/appmovil/model/ContactModel.kt
+++ app/src/main/java/cr/ac/utn/appmovil/model/ContactModel.kt
@@ -1,13 +1,20 @@
 package cr.ac.utn.appmovil.model
-
 import android.content.Context
+import android.content.res.Resources
 import cr.ac.utn.appmovil.contactmanager.R
+import cr.ac.utn.appmovil.data.DBContactManager
 import cr.ac.utn.appmovil.identities.Contact
-import cr.ac.utn.appmovil.interfaces.IDBManager
-import cr.ac.utn.appmovil.data.SQLiteManager
+import cr.ac.utn.appmovil.identities.ContactDBManager
 
-class ContactModel(private val context: Context) {
-    private var dbManager: IDBManager = SQLiteManager(context)
+class ContactModel(context: Context) {
+
+    private lateinit var _context: Context
+    private var dbManager: ContactDBManager
+
+    init {
+        _context = context  // Inicializar _context dentro del bloque init
+        dbManager = DBContactManager(_context)  // Inicializar dbManager después de que _context esté inicializado
+    }
 
     fun addContact(contact: Contact) {
         dbManager.add(contact)
@@ -18,9 +25,9 @@ class ContactModel(private val context: Context) {
     }
 
     fun removeContact(id: String) {
-        val result = dbManager.getById(id)
+        val result = dbManager.getByid(id)
         if (result == null)
-            throw Exception(context.getString(R.string.msgNotFoundContact))
+            throw Exception(Resources.getSystem().getString(R.string.msgNotFoundContact))
 
         dbManager.remove(id)
     }
@@ -28,9 +35,9 @@ class ContactModel(private val context: Context) {
     fun getContacts() = dbManager.getAll()
 
     fun getContact(id: String): Contact {
-        val result = dbManager.getById(id)
+        val result = dbManager.getByid(id)
         if (result == null) {
-            val message = context.getString(R.string.msgNotFoundContact)
+            val message = _context.getString(R.string.msgNotFoundContact).toString()
             throw Exception(message)
         }
         return result
